bdab554078d23f94a5bc537949951abe4aafcae7
zhhsh <33864086+zhhsh@users.noreply.github.com>
2026-06-02 16:33:41 +0800

    Add UI

--- script.js
+++ script.js
@@ -33,70 +33,85 @@ function getHumanChoice() {
 // test getHumanChoice
 // console.log(getHumanChoice())
 
+// players' scores
+let humanScore = 0;
+let computerScore = 0;
+
+// one round play, take two choices as params, update winner's score
+function playRound(humanChoice, computerChoice) {
+	// format humanChoice into lower case
+	humanChoice = humanChoice.toLowerCase()
+
+	// decide the winner from the choices
+	let winner; // winner of players
+	let result; // String to show the result //  “You lose! Paper beats Rock”.
+	if (humanChoice == "rock") {
+		if (computerChoice == "paper") {
+			winner = "computer";
+			result = `You lose! ${computerChoice} beats ${humanChoice}`
+		} else if (computerChoice == "scissors") {
+			winner = "human";
+			result = `You win! ${humanChoice} beats ${computerChoice}`
+		} else {
+			winner = "no winner"
+			result = "Tie! No winner"
+		}
+	} else if (humanChoice == "paper") {
+		if (computerChoice == "rock") {
+			winner = "human";
+			result = `You win! ${humanChoice} beats ${computerChoice}`
+		} else if (computerChoice == "scissors") {
+			winner = "computer";
+			result = `You lose! ${computerChoice} beats ${humanChoice}`
+		} else {
+			winner = "no winner"
+			result = "Tie! No winner"
+		}
+	} else if (humanChoice == "scissors") {
+		if (computerChoice == "rock") {
+			winner = "computer";
+			result = `You lose! ${computerChoice} beats ${humanChoice}`
+		} else if (computerChoice == "paper") {
+			winner = "human";
+			result = `You win! ${humanChoice} beats ${computerChoice}`
+		} else {
+			winner = "no winner"
+			result = "Tie! No winner"
+		}
+	}
+
+	// display the result
+	// console.log(result);
+	document.querySelector('#result').textContent = result;
+
+	// update winner's score if there's a winner
+	switch (winner) {
+		case "human":
+			humanScore++;
+			break;
+		case "computer":
+			computerScore++;
+			break;
+	}
+	document.querySelector('#user-score').textContent = humanScore;
+	document.querySelector('#computer-score').textContent = computerScore;
+	if (humanScore >= 5) {
+		document.querySelector('#result').textContent = 'User Win';
+		document.querySelector('#rps-panel').removeEventListener('click', clickHandler)
+		document.querySelector('#reset-btn').style.display = "inline";
+	} else if (computerScore >= 5) {
+		document.querySelector('#result').textContent = 'Computer Win';
+		document.querySelector('#rps-panel').removeEventListener('click', clickHandler);
+		document.querySelector('#reset-btn').style.display = "inline";
+	}
+
+
+
+	return winner, result;
+}
 
 // play 5 rounds
 function playGame() {
-
-	// players' scores
-	let humanScore = 0;
-	let computerScore = 0;
-
-	// one round play, take two choices as params, update winner's score
-	function playRound(humanChoice, computerChoice) {
-		// format humanChoice into lower case
-		humanChoice = humanChoice.toLowerCase()
-
-		// decide the winner from the choices
-		let winner; // winner of players
-		let result; // String to show the result //  “You lose! Paper beats Rock”.
-		if (humanChoice == "rock") {
-			if (computerChoice == "paper") {
-				winner = "computer";
-				result = `You lose! ${computerChoice} beats ${humanChoice}`
-			} else if (computerChoice == "scissors") {
-				winner = "human";
-				result = `You win! ${humanChoice} beats ${computerChoice}`
-			} else {
-				winner = "no winner"
-				result = "Tie! No winner"
-			}
-		} else if (humanChoice == "paper") {
-			if (computerChoice == "rock") {
-				winner = "human";
-				result = `You win! ${humanChoice} beats ${computerChoice}`
-			} else if (computerChoice == "scissors") {
-				winner = "computer";
-				result = `You lose! ${computerChoice} beats ${humanChoice}`
-			} else {
-				winner = "no winner"
-				result = "Tie! No winner"
-			}
-		} else if (humanChoice == "scissors") {
-			if (computerChoice == "rock") {
-				winner = "computer";
-				result = `You lose! ${computerChoice} beats ${humanChoice}`
-			} else if (computerChoice == "paper") {
-				winner = "human";
-				result = `You win! ${humanChoice} beats ${computerChoice}`
-			} else {
-				winner = "no winner"
-				result = "Tie! No winner"
-			}
-		}
-
-		// display the result
-		console.log(result);
-
-		// update winner's score if there's a winner
-		switch (winner) {
-			case "human":
-				humanScore++;
-				break;
-			case "computer":
-				computerScore++;
-				break;
-		}
-	}
 
 	// test getHumanChoice
 	// const humanChoice = getHumanChoice();
@@ -120,4 +135,33 @@ function playGame() {
 	}
 }
 
-playGame();
+// playGame();
+
+function clickHandler(e) {
+	switch (e.target.textContent) {
+		case 'rock':
+			console.log('rock');
+			playRound('rock', getComputerChoice());
+			break;
+		case 'paper':
+			console.log('paper');
+			playRound('paper', getComputerChoice());
+			break;
+		case 'scissors':
+			console.log('scissors');
+			playRound('scissors', getComputerChoice());
+			break;
+	}
+}
+
+function resetHandler(e) {
+	humanScore = 0;
+	computerScore = 0;
+	document.querySelector('#user-score').textContent = humanScore;
+	document.querySelector('#computer-score').textContent = computerScore;
+	document.querySelector('#rps-panel').addEventListener('click', clickHandler);
+	document.querySelector('#reset-btn').style.display = "none";
+}
+
+document.querySelector('#rps-panel').addEventListener('click', clickHandler);
+document.querySelector('#reset-btn').addEventListener('click', resetHandler);
